9595a28cc3293f4c3ef8726496d0c54e2d009adc
Gibado <tssquirtle@gmail.com>
2019-07-10 11:23:33 -0500

    Added comments and simplify the code

--- scripts/cookie-cleanup.user.js
+++ scripts/cookie-cleanup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         cookie-cleanup.user.js
 // @namespace    https://github.com/Gibado
-// @version      2019.7.10
+// @version      2019.7.10.1
 // @description  Adds a function to the site delete all ""gsScrollPos" cookies from that site.  This is is an issue with some Google sites.
 // @author       Tyler Studanski
 // @match        https://www.google.com/*
@@ -11,6 +11,7 @@
 // ==/UserScript==
 
 // ==Version History==
+// 2019.7.10.1 - Simplify code and added comments
 // 2019.7.10 - Initial version
 // ==/Version History==
 
@@ -20,17 +21,19 @@
     // Your code here...
     document.gibado = {
         deleteCookie : function(name) {
-          document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+            // Sets the cookie expiration to a past time
+            document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
         },
         verifyCookie : function(cookie) {
+            // Filter function to find problem gsScrollPos cookies
             return cookie.startsWith('gsScrollPos');
         },
         cleanCookies : function() {
-            var cookies = document.cookie.split('; ');
-            cookies = cookies.filter(document.gibado.verifyCookie);
+            // Get all cookies and filter to only problem cookies
+            var cookies = document.cookie.split('; ').filter(document.gibado.verifyCookie);
+            // Delete each cookie
             for (var i = 0; i < cookies.length; i++) {
-                var cookieName = cookies[i].split('=')[0];
-                document.gibado.deleteCookie(cookieName);
+                document.gibado.deleteCookie(cookies[i].split('=')[0]);
             }
         }
     };
