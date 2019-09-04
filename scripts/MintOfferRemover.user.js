9d201af09cc13becc3305214282ff65d578b20aa
Gibado <tssquirtle@gmail.com>
2019-09-04 11:31:59 -0500

    Split out offers and advice

--- scripts/MintOfferRemover.user.js
+++ scripts/MintOfferRemover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		Mint Offer Remover
 // @namespace	https://github.com/Gibado
-// @version		2019.9.4.3
+// @version		2019.9.4.4
 // @description	Removes the offers on the page
 // @match		https://mint.intuit.com/overview.event
 // @require		https://raw.githubusercontent.com/Gibado/UserScriptRepository/master/common/Utils.js
@@ -13,6 +13,7 @@
 // ==/UserScript==
 
 // ==Version History==
+// 2019.9.4.4 - Split out offers and advice
 // 2019.9.4.3 - Added documentation
 // 2019.9.4.2 - Changed conditional function
 // 2019.9.4.1 - Added Utils and common Mint functions
@@ -33,12 +34,19 @@
     }
 
     /**
-     * Checks if offers exist on the page
-     * @return {boolean} Returns true if offers exist on the page
+     * Checks if offers exists on the page
+     * @return {boolean} Returns true if offers exists on the page
      */
-    document.gibado.Mint.offersExist = function() {
-        return document.getElementsByClassName('offerSection').length > 0 &&
-            document.getElementsByClassName('CardView adviceWidget').length > 0;
+    document.gibado.Mint.offersExists = function() {
+        return document.getElementsByClassName('offerSection').length > 0;
+    };
+
+    /**
+     * Checks if advice exists on the page
+     * @return {boolean} Returns true if advice exists on the page
+     */
+    document.gibado.Mint.adviceExists = function() {
+        return document.getElementsByClassName('CardView adviceWidget').length > 0;
     };
 
     /**
@@ -47,9 +55,17 @@
     document.gibado.Mint.clearOffers = function() {
         var offer = document.getElementsByClassName('offerSection')[0];
         offer.hidden = true;
-        offer = document.getElementsByClassName('CardView adviceWidget')[0];
+        document.gibado.Mint.clearAdvice();
+    };
+
+    /**
+     * Hides advice on the page
+     */
+    document.gibado.Mint.clearAdvice = function() {
+        var offer = document.getElementsByClassName('CardView adviceWidget')[0];
         offer.hidden = true;
     };
 
-    Utils.conditionalRun(2000, document.gibado.Mint.offersExist, document.gibado.Mint.clearOffers);
+    Utils.conditionalRun(2000, document.gibado.Mint.offersExists, document.gibado.Mint.clearOffers);
+    Utils.conditionalRun(2000, document.gibado.Mint.adviceExists, document.gibado.Mint.clearAdvice);
 })();
