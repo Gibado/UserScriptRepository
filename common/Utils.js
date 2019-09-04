60507500ded30225df45d4d170f405eed2414529
Gibado <tssquirtle@gmail.com>
2019-09-04 10:49:24 -0500

    Started a generic Utils.js with a conditional run method

---
+++ common/Utils.js
@@ -0,0 +1,19 @@
+if (Utils === undefined) {
+    var Utils = {};
+}
+
+/**
+ * Checks on an interval if a condition is meet and then executes a callback function
+ * @param checkTime How long to wait (in milliseconds) between checking the condition
+ * @param condition Function that returns a boolean to determine if the callback function should be called
+ * @param callback Function to execute when the conditions are meet
+ */
+Utils.conditionalRun = function(checkTime, condition, callback) {
+    setTimeout(function() {
+        if (condition()) {
+            callback();
+        } else {
+            Utils.conditionalRun(checkTime, condition, callback);
+        }
+    }, checkTime);
+};
