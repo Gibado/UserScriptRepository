2bbfa1cc330787c5908cdc6535e2f58f583aaf04
Gibado <tssquirtle@gmail.com>
2019-09-04 11:26:53 -0500

    Added intervalConditionalRun

--- common/Utils.js
+++ common/Utils.js
@@ -17,3 +17,17 @@ Utils.conditionalRun = function(checkTime, condition, callback) {
         }
     }, checkTime);
 };
+
+/**
+ * Checks on a continuous interval if a condition is meet and then executes a callback function
+ * @param checkTime How long to wait (in milliseconds) between checking the condition
+ * @param condition Function that returns a boolean to determine if the callback function should be called
+ * @param callback Function to execute when the conditions are meet
+ */
+Utils.intervalConditionalRun = function (checkTime, condition, callback) {
+    setInterval(function() {
+        if (condition()) {
+            callback();
+        }
+    }, checkTime);
+};
