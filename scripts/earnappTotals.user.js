a85f260ee393b7e9204925ee551e1d08dd28ae8e
Tyler <tssquirtle@gmail.com>
2022-04-12 14:41:54 -0500

    Update earnappTotals.user.js
    Added site validation before attempting to add the results.

--- scripts/earnappTotals.user.js
+++ scripts/earnappTotals.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EarnApp Daily Totals
 // @namespace    https://github.com/Gibado
-// @version      2022.04.12.0
+// @version      2022.04.12.4
 // @description  Adds daily earned totals under the data graph
 // @author       Tyler Studanski
 // @match        https://earnapp.com/dashboard
@@ -17,18 +17,18 @@ document.myEarnApp = function() {
 	var self = this;
 	self.usageData = undefined;
 	self.dailyMap = {};
-	
+
 	self.grabUsage = function() {
 		// Reset data
 		self.usageData = undefined;
 		self.dailyMap = {};
-		
+
 		$.ajax({
 			'url' : 'https://earnapp.com/dashboard/api/usage?appid=earnapp_dashboard&version=1.292.111&step=daily',
 			'type' : 'GET',
 			'success' : function(data) {
 				self.usageData = data;
-				self.processUsage(self.usageData);				
+				self.processUsage(self.usageData);
 			},
 			'error' : function(request,error)
 			{
@@ -36,28 +36,29 @@ document.myEarnApp = function() {
 			}
 		});
 	};
-	
+
+
+
 	self.processUsage = function(usageData) {
 		if (usageData === undefined) {
 			return;
 		}
-		
 		for (const machine of usageData) {
 			//console.log(machine.name + ' used ' + machine.data);
 			for (const date in machine.data) {
 				console.log(date + ' - ' + machine.data[date]);
 				if (self.dailyMap[date] === undefined) {
-					self.dailyMap[date] = { 
+					self.dailyMap[date] = {
 						bytes: 0,
 					};
 				}
-				
+
 				self.dailyMap[date].bytes += machine.data[date];
 			}
 		}
 		self.updateCounts(self.dailyMap);
-	};
-	
+    };
+
 	self.updateCounts = function(dateMap) {
 		for (const dateKey in dateMap) {
 			var date = dateMap[dateKey];
@@ -68,33 +69,35 @@ document.myEarnApp = function() {
 		}
 		self.addToPage(dateMap);
 	};
-	
+
 	self.addToPage = function(results) {
+
 		var display = self.getDisplay();
-		
+
 		for(;display.childElementCount > 0;) {
 			display.childNodes[0].remove();
 		}
-		
+
 		display.appendChild(self.buildTable(results));
-		
+
 		self.printResults();
-	};
-	
+    };
+
 	self.getDisplay = function() {
 		var display = document.getElementById('earningDisplay');
 		if (display !== null) {
 			return display;
 		}
+
 		// Build display
 		var landing = document.getElementsByClassName('ea_usage_chart')[0];
 		display = document.createElement('div');
 		display.id = 'earningDisplay';
-		
+
 		landing.appendChild(display);
 		return display;
 	};
-	
+
 	self.buildTable = function(results) {
 		var table = document.createElement('table');
 		// Add headers
@@ -103,28 +106,46 @@ document.myEarnApp = function() {
 		// Add data
 		var dataRow = document.createElement('tr');
 		table.appendChild(dataRow);
-		
+
 		//for (const date in results) {
 		Object.keys(results).reverse().forEach(function(date) {
-			
+
 			var header = document.createElement('th');
 			header.textContent = date;
 			headers.appendChild(header);
-			
+
 			var data = document.createElement('td');
 			data.textContent = results[date].earnings;
 			dataRow.appendChild(data);
 		});
 		//}
-		
+
 		return table;
 	};
-	
+
 	self.printResults = function() {
 		console.log(self.usageData);
 		console.log(self.dailyMap);
 	};
-	
+
+    // Checks if this is the correct page and waits the intervalTime before checking again
+	// intervalTime: time in milliseconds before each check
+	self.delayedRun = function(intervalTime) {
+		setTimeout(function(){
+            if (!self.validateSite()) {
+				self.delayedRun(intervalTime);
+			} else {
+				self.grabUsage();
+			}
+		}, intervalTime);
+	};
+
+    // Holds off on further processing until the page is ready
+    self.validateSite = function() {
+        console.log('Checking for valid site...');
+        return document.getElementsByClassName('ea_usage_chart').length > 0;
+    };
+
 	// Copied from https://stackoverflow.com/a/16233919/3416155
 	// Create our number formatter.
 	self.formatter = new Intl.NumberFormat('en-US', {
@@ -135,11 +156,11 @@ document.myEarnApp = function() {
 	  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
 	  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 	});
-	
+
 	return self;
 };
 
 setTimeout(function() {
 	var myEarnApp = document.myEarnApp();
-	myEarnApp.grabUsage();
-}, 500);
+	myEarnApp.delayedRun(1000);
+}, 10);
