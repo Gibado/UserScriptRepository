57dd29bc617ac2004d0a38b60b66ba5a2d466cfc
Tyler <tssquirtle@gmail.com>
2022-04-26 09:36:22 -0500

    Update hereComesTheBus.user.js
    - Fixed icon information
    - Removed excess whitespace

--- scripts/hereComesTheBus.user.js
+++ scripts/hereComesTheBus.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Here Comes The Bus Child switch
 // @namespace    https://github.com/Gibado
-// @version      2022.4.26.0
+// @version      2022.4.26.1
 // @description  Automatically picks a different child based on the time of day
 // @author       Tyler Studanski
 // @match        https://login.herecomesthebus.com/Map.aspx
-// @icon         https://www.google.com/s2/favicons?sz=64&herecomesthebus.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=herecomesthebus.com
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @downloadURL  https://github.com/Gibado/UserScriptRepository/raw/master/scripts/hereComesTheBus.user.js
 // @updateURL    https://github.com/Gibado/UserScriptRepository/raw/master/scripts/hereComesTheBus.user.js
@@ -17,7 +17,7 @@ class TimeOfDay {
 	static AM = new TimeOfDay("AM");
 	static MID = new TimeOfDay("Mid Day");
 	static PM = new TimeOfDay("PM");
-	
+
 	constructor(name) {
 		this.name = name;
 	}
@@ -28,8 +28,7 @@ document.HCTB = function () {
 	self.timeOfDay = null;
 	self.passenger = null;
 	self.desiredPmPassengerValue = '3c8d1875-f11d-4125-b605-420670067c42';
-	
-	
+
 	// Main function call
 	self.run = function() {
 		// Check the time of day
@@ -38,17 +37,18 @@ document.HCTB = function () {
 		self.printData();
 		self.updatePassenger();
 	};
-	
+
 	// Debug function
 	self.printData = function() {
 		console.log('Currently set to ' + self.timeOfDay.name);
 		console.log('Selected passenger is: ' + self.prettyPrintPassenger(self.passenger));
 	};
-	
+
+	// Prints out passenger information
 	self.prettyPrintPassenger = function(passengerOption) {
 		return passengerOption.text + '(' + passengerOption.value + ')'
 	};
-	
+
 	// Finds the currently set time of day
 	self.getTimeOfDay = function() {
 		var time = $('#pickTimeOfDay')[0].getElementsByTagName('select')[0].selectedIndex;
@@ -62,26 +62,26 @@ document.HCTB = function () {
 		}
 		return null;
 	};
-	
+
 	// Finds the currently selected passenger
 	self.getPassenger = function() {
 		var passengerDropDown = $('#pickPassenger')[0].getElementsByTagName('select')[0];
 		return passengerDropDown[passengerDropDown.selectedIndex];
 	};
-	
+
 	// If the time is PM then change selected passenger
 	self.updatePassenger = function() {
 		if (self.timeOfDay == TimeOfDay.PM && self.passenger.value != self.desiredPmPassengerValue) {
 			var passengerDropDown = $('#pickPassenger')[0].getElementsByTagName('select')[0];
 			passengerDropDown.value = self.desiredPmPassengerValue;
-			
+
 			var newPassenger = self.getPassenger();
 			console.log('Changed passenger from ' + self.prettyPrintPassenger(self.passenger) + ' to ' + self.prettyPrintPassenger(newPassenger));
 		} else {
 			console.log('Passenger is correct.  No action needed.'); // TODO - Remove later
 		}
 	};
-	
+
 	return self;
 };
 
